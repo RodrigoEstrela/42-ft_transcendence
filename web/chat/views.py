f90a8a8543e048f267ca53555dec98fa1439685d
rdas-nev <restrela20@gmail.com>
2023-12-16 01:31:23 +0000

    sort chat msgs by timestamp

--- web/chat/views.py
+++ web/chat/views.py
@@ -10,10 +10,10 @@ def private_room(request, other_user):
     other_user_obj = User.objects.get(username=other_user)
     if ChatRoom.objects.filter(user1=request.user, user2=other_user_obj).first():
         room_obj = ChatRoom.objects.filter(user1=request.user, user2=other_user_obj).first()
-        chats = Chat.objects.filter(room=room_obj)
+        chats = Chat.objects.filter(room=room_obj).order_by('timestamp')
     elif ChatRoom.objects.filter(user2=request.user, user1=other_user_obj).first():
         room_obj = ChatRoom.objects.filter(user2=request.user, user1=other_user_obj).first()
-        chats = Chat.objects.filter(room=room_obj)
+        chats = Chat.objects.filter(room=room_obj).order_by('timestamp')
     else:
         room_obj = ChatRoom(user1=request.user, user2=other_user_obj, name=f"{request.user.username}_{other_user}")
         room_obj.save()
